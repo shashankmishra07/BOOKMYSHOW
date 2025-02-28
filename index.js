525978dcac87bbd2c7fa26de5e5a3d54f1d14ea7
shashankmishra07 <iamshashankmishra2003@gmail.com>
2025-02-28 15:41:02 +0530

    reer

---
+++ index.js
@@ -0,0 +1,49 @@
+function signup(){
+    let sname= document.querySelector('#s_name').value
+    let slname= document.querySelector('#s_lname').value
+    let semail= document.querySelector('#s_email').value
+    let spass= document.querySelector('#s_pass').value
+    let scpass= document.querySelector('#s_cpass').value
+
+    localStorage.setItem("frist name",sname)
+    localStorage.setItem("last name",slname)
+let email=    localStorage.setItem("s_email",semail)
+let password=  localStorage.setItem("s_password",spass)
+    localStorage.setItem("s_conform_password",scpass)
+    return false
+
+
+}
+
+function login(){
+    let lemail=document.querySelector("#l_email").value
+     let lpass=document.querySelector("#l_pass").value
+
+
+   
+
+    
+
+  let semail=localStorage.getItem("s_email")
+  let spass=localStorage.getItem("s_password")
+  
+  if (lemail != semail || lpass!=spass) {
+    alert("user undefiend")
+    return false
+}
+  
+  
+if (data.email != logindata.loginemail || data.password != logindata.loginpassword) {
+    alert("user undefiend")
+    return false
+}
+}
+
+// admin data
+
+function admin(){
+    let e=document.querySelector("#admin_email").value
+    let p=document.querySelector("#admin_pass").value
+
+    localStorage.setItem("Admin Email")
+}
